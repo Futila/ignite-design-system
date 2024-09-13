1f5f47d7ddc1dbf316de9304def3aeda871dff52
Fernando Futila <fernando.futila@gmail.com>
2024-09-13 11:18:10 -0300

    feat: configurando tokens

---
+++ packages/tokens/src/radii.ts
@@ -0,0 +1,10 @@
+
+// For border radius it's recommended to use "px" instead of "rem"
+export const radii = {
+  px: '1px',
+  xs: '4px',
+  sm: '6px',
+  md: '8px',
+  lg: '16px',
+  full: '99999px',
+}
